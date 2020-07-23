a8bb18f778724531013795a2d32e4b98c055eb09
cintiahet <cintiahetzer@gmail.com>
2020-07-23 18:03:03 -0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 const newman = require('newman');
 
-let collections = ['./collections/BookingPractice1.postman_collection.json','/collections/BookingPractice2.postman_collection.json]', './collections/BookingPractice3.postman_collection.json', './collections/BookingPractice4.postman_collection.json', './collections/BookingPractice6.postman_collection.json'];
+let collections = ['./collections/BookingPractice1.postman_collection.json','/collections/BookingPractice2.postman_collection.json', './collections/BookingPractice3.postman_collection.json', './collections/BookingPractice4.postman_collection.json', './collections/BookingPractice6.postman_collection.json'];
 const env = require('./environments/BookingLive.postman_environment.json');
 
 (function runCollection(i = 0) {
