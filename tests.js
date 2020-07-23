9a6dfb69afe100245b973be869b32d2004f4c176
cinhet <cintiahetzer@gmail.com>
2020-07-23 18:30:48 -0300

    adding tests junit conf

--- tests.js
+++ tests.js
@@ -1,18 +1,19 @@
 const newman = require('newman');
 
-let collections = ['./collections/BookingPractice1.postman_collection.json','/collections/BookingPractice2.postman_collection.json', './collections/BookingPractice3.postman_collection.json', './collections/BookingPractice4.postman_collection.json', './collections/BookingPractice6.postman_collection.json'];
+let collections = ['./collections/BookingPractice1.postman_collection.json','./collections/BookingPractice3.postman_collection.json', './collections/BookingPractice3.postman_collection.json', './collections/BookingPractice4.postman_collection.json', './collections/BookingPractice6.postman_collection.json'];
 const env = require('./environments/BookingLive.postman_environment.json');
 
 (function runCollection(i = 0) {
     newman.run({
         collection: collections[i],
         environment: env,
-        reporters: 'cli',
+        reporters: ['cli', 'junit'],
+        reporter: { junit : { export : './result.xml.xml' } },
         iterationCount: 1,
     }, function (err) {
         if(err){ throw err; }
         i++;
-        if(i<collections.length){
+        if(i < collections.length){
             runCollection(i);
         }
         else{
