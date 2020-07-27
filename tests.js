8b2ff2cbd870e26fb0c0f953660318e582068aea
cinhet <cintiahetzer@gmail.com>
2020-07-27 12:56:46 -0300

    fixes

--- tests.js
+++ tests.js
@@ -1,14 +1,15 @@
 const newman = require('newman');
 
 let collections = ['./collections/BookingPractice1.postman_collection.json','./collections/BookingPractice2.postman_collection.json', './collections/BookingPractice3.postman_collection.json', './collections/BookingPractice4.postman_collection.json', './collections/BookingPractice6.postman_collection.json'];
-const env = require('./environments/BookingLive.postman_environment.json');
+
+const env = require(process.argv[2]);
 
 (function runCollection(i = 0) {
     newman.run({
         collection: collections[i],
         environment: env,
         reporters: ['cli', 'junit'],
-        reporter: { junit : { export : './reports/'+collections[i]+'_results.xml' } },
+        reporter: { junit : { export : './reports'+collections[i]+'_results.xml' } },
         iterationCount: 1,
     }, function (err) {
         if(err){ throw err; }
